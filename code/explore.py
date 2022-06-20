14ee0e790d6cc728470395047326520985e68d0a
Samuele Ancora <samueleancora@MacBook-Pro-di-Samuele.local>
2022-06-20 11:20:22 +0200

    Updated links

--- code/explore.py
+++ code/explore.py
@@ -69,3 +69,9 @@ def plotScatterMatrix(df, plotSize, textSize):
                           va='center', size=textSize)
     plt.suptitle('Scatter and Density Plot')
     plt.show()
+
+
+plotPerColumnDistribution(pd.DataFrame({"A": [14, 4, 5, 4, 1],
+                                        "B": [5, 2, 54, 3, 2],
+                                        "C": [20, 20, 7, 3, 8],
+                                        "D": [14, 3, 6, 2, 6]}), 2, 2)
